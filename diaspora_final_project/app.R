3213ad95c98503b0311fa2ba0c66453ae9e519f9
Cristopher Patvakanian <cris@Cristophers-MBP.55Waverly717>
2019-12-02 04:25:42 -0500

    a lot of progress

---
+++ diaspora_final_project/app.R
@@ -0,0 +1,35 @@
+# Here I am loading in the library
+
+library(shiny)
+
+# The ui specifies what we are going to output for the graphics,
+# and in this specific case I am saying I want to have the
+# website show a title, and two pages which say graphic and about
+
+ui <- navbarPage("Armenian Diaspora Project",
+                 tabPanel("Community Information", fluidPage(imageOutput('map'))),
+                 tabPanel("Diaspora Data Highlights", fluidPage(imageOutput('map'))),
+                 tabPanel("What Influences Number of Communities", fluidPage(imageOutput('map'))),
+                 tabPanel("About", fluidPage(textOutput('text'))))
+
+
+
+# Here, server defines what that output we specified earlier will be
+# and now we are actually assigning that output to be our graphic
+# and our description for the about.
+
+server <- function(input, output) {
+    output$map <- renderImage({
+        list(src = "gif.gif", contentType = "image/gif")
+    }, deleteFile = FALSE
+    )
+    output$text <- renderText({
+        "The graphic here is of a map which shows stops of red toyotas in Oklahoma, by date ."
+    })
+}
+
+# Here I am running the output. This is how
+# my actual app is run because we are calling the 
+# information we had previously
+
+shinyApp(ui = ui, server = server)
